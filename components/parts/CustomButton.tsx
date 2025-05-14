943be8e1b44b9a8c7843716876ee4c1c855c0d8f
saito3215 <kazuto.saito@fox-hound.jp>
2025-05-14 12:44:30 +0900

    タスク1-1完了

---
+++ components/parts/CustomButton.tsx
@@ -0,0 +1,36 @@
+// components/parts/CustomButton.tsx
+import React from "react";
+import { Button, ButtonProps } from "@mui/material";
+
+interface CustomButtonProps extends ButtonProps {
+  variantType?: "primary" | "secondary" | "danger";
+}
+
+const CustomButton: React.FC<CustomButtonProps> = ({
+  variantType = "primary",
+  ...props
+}) => {
+  let color: ButtonProps["color"] = "primary";
+
+  switch (variantType) {
+    case "primary":
+      color = "primary";
+      break;
+    case "secondary":
+      color = "success";
+      break;
+    case "danger":
+      color = "warning";
+      break;
+    default:
+      color = "primary";
+  }
+
+  return (
+    <Button color={color} variant={props.variant} {...props}>
+      {props.children}
+    </Button>
+  );
+};
+
+export default CustomButton;
